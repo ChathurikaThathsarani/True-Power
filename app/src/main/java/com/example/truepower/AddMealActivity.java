2add624033a124be3f891cd58d6cf5aedfbf7de8
Thamindu Chankana Gamage <it20604530@my.sliit.lk>
2022-04-16 15:43:19 +0530

    Implemented register account page

--- app/src/main/java/com/example/truepower/AddMealActivity.java
+++ app/src/main/java/com/example/truepower/AddMealActivity.java
@@ -1,13 +1,17 @@
 package com.example.truepower;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,6 +57,45 @@ public class AddMealActivity extends AppCompatActivity implements AdapterView.On
 
         mealTypeDataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mealType.setAdapter(mealTypeDataAdapter);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_home);
+        bottomNavigationView.setSelectedItemId(R.id.meal);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        overridePendingTransition(0, 0);
+                        finish();
+                        return true;
+                    case R.id.health:
+                        startActivity(new Intent(getApplicationContext(), HealthActivity.class));
+                        overridePendingTransition(0, 0);
+                        finish();
+                        return true;
+                    case R.id.meal:
+                        startActivity(new Intent(getApplicationContext(), MealActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.routine:
+                        startActivity(new Intent(getApplicationContext(), RoutineActivity.class));
+                        overridePendingTransition(0, 0);
+                        finish();
+                        return true;
+                    case R.id.workout:
+                        startActivity(new Intent(getApplicationContext(), WorkoutActivity.class));
+                        overridePendingTransition(0, 0);
+                        finish();
+                        return true;
+                }
+
+                return false;
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/truepower/RegisterActivity.java
+++ app/src/main/java/com/example/truepower/RegisterActivity.java
@@ -5,11 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthActionCodeException;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText name;
+    private EditText age;
+    private EditText email;
+    private EditText password;
+    private Button signUp;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +36,6 @@ public class RegisterActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_start);
         bottomNavigationView.setSelectedItemId(R.id.item_register);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-
 
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -40,5 +57,50 @@ public class RegisterActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        name = findViewById(R.id.et_reg_name);
+        age = findViewById(R.id.et_reg_age);
+        email = findViewById(R.id.email_reg_email);
+        password = findViewById(R.id.pw_reg_password);
+        signUp = findViewById(R.id.btn_sign_up);
+
+        auth = FirebaseAuth.getInstance();
+
+        if(auth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            finish();
+        }
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_name = name.getText().toString().trim();
+                String txt_age = age.getText().toString().trim();
+                String txt_email = email.getText().toString().trim();
+                String txt_password = password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(txt_name)||TextUtils.isEmpty(txt_age)||TextUtils.isEmpty(txt_email)||TextUtils.isEmpty(txt_password)){
+                    Toast.makeText(RegisterActivity.this, "Please Fill All The Required Fields !!!", Toast.LENGTH_SHORT).show();
+                }
+                if(txt_password.length()<6){
+                    Toast.makeText(RegisterActivity.this, "Password Too Short !!!", Toast.LENGTH_SHORT).show();
+                }
+                    auth.createUserWithEmailAndPassword(txt_email,txt_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "User Registration Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                            }
+                            else{
+                                Toast.makeText(RegisterActivity.this,"User Registration Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+            }
+        });
     }
+
+
 }
