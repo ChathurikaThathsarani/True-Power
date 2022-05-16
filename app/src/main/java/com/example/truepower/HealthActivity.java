cd51cf327ff41203372b92ac8f87d00ba1b462fc
kithmi <it20298876@my.sliit.lk>
2022-05-16 21:03:43 +0530

    Implement health functionaliities

--- app/src/main/java/com/example/truepower/HealthActivity.java
+++ app/src/main/java/com/example/truepower/HealthActivity.java
@@ -2,14 +2,30 @@ package com.example.truepower;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HealthActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private DatabaseReference health;
+    HealthAdapter adapter;
+    private FirebaseAuth auth;
+
+    private String obid="";
+    private String name="";
+    private String nextDate="";
+    private String description="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,5 +68,30 @@ public class HealthActivity extends AppCompatActivity {
                 return false;
             }
         });
+        auth=FirebaseAuth.getInstance();
+        health = FirebaseDatabase.getInstance().getReference().child("health").child(auth.getCurrentUser().getUid());
+        recyclerView = findViewById(R.id.health_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        FirebaseRecyclerOptions<Health> options = new FirebaseRecyclerOptions.Builder<Health>().setQuery(health, Health.class).build();
+        adapter = new HealthAdapter(options);
+        recyclerView.setAdapter(adapter);
+    }
+    @Override protected void onStart()
+    {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    // Function to tell the app to stop getting
+    // data from database on stopping of the activity
+    @Override protected void onStop()
+    {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    public void addHealth(View view) {
+        Intent intent = new Intent(this, AddHealth.class);
+        startActivity(intent);
     }
 }
